22c2a9a218c1679a2e66dddfc94c6a77808d50f3
Andrei Vulpe <andreivulpe02@yahoo.com>
2022-09-26 20:05:12 +0300

    Do Question Model

---
+++ src/main/java/com/dbproject/cvapp/service/QuestionService.java
@@ -0,0 +1,26 @@
+package com.dbproject.cvapp.service;
+
+import com.dbproject.cvapp.model.Question;
+import com.dbproject.cvapp.repository.QuestionRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class QuestionService {
+
+    private final QuestionRepository questionRepository;
+
+    // Method to add a question to the db
+    public void createQuestion(Question question) {
+        questionRepository.save(question);
+    }
+
+    // Method to get all questions
+    public List<Question> getAllQuestions() {
+        return new ArrayList<>(questionRepository.findAll());
+    }
+}
