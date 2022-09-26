e8e0b415e74407bdcd38ac637a2d54dddaa20c62
Andrei Vulpe <andreivulpe02@yahoo.com>
2022-09-26 16:35:43 +0300

    Do Recommendation Model

--- src/main/java/com/dbproject/cvapp/controller/RecommendationController.java
+++ src/main/java/com/dbproject/cvapp/controller/RecommendationController.java
@@ -1,13 +1,36 @@
 package com.dbproject.cvapp.controller;
 
+import com.dbproject.cvapp.exception.RecommendationNotFoundException;
+import com.dbproject.cvapp.model.Recommendation;
+import com.dbproject.cvapp.model.Status;
 import com.dbproject.cvapp.service.RecommendationService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("recommendation")
 public class RecommendationController {
     private final RecommendationService recommendationService;
 
+    @GetMapping("all")
+    public List<Recommendation> getAllRecommendations() {
+        return recommendationService.getAllRecommendations();
+    }
 
+    @GetMapping("all/{id}")
+    public List<Recommendation> getRecommendationsById(@PathVariable Integer id) {
+        return recommendationService.getRecommendationsById(id);
+    }
+
+    // TODO: ADD RECOMMENDATION
+    // @Vulpe: Nu mai stiu daca stia sa extraga springul singur un obiect
+
+    @PostMapping("changeStatus/{id}/{status}")
+    public void changeRecommendationStatus(@PathVariable Status status, @PathVariable Integer id)
+            throws RecommendationNotFoundException {
+        recommendationService.changeRecommendationStatus(status, id);
+    }
 }
--- src/main/java/com/dbproject/cvapp/service/RecommendationService.java
+++ src/main/java/com/dbproject/cvapp/service/RecommendationService.java
@@ -7,20 +7,42 @@ import com.dbproject.cvapp.repository.RecommendationRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class RecommendationService {
     private final RecommendationRepository recommendationRepository;
 
+    // Method to change the status of a recommendation
     public void changeRecommendationStatus(Status progressStatus, Integer recommendationId)
             throws RecommendationNotFoundException {
         Optional<Recommendation> tmpOptionalRecommendation = recommendationRepository.findById(recommendationId);
         if(tmpOptionalRecommendation.isEmpty()) {
             throw new RecommendationNotFoundException();
         } else {
-            tmpOptionalRecommendation.get().setProgressStatus(progressStatus);
+            Recommendation tmpRecommendation = tmpOptionalRecommendation.get();
+            tmpRecommendation.setProgressStatus(progressStatus);
+            recommendationRepository.save(tmpRecommendation);
         }
+    }
+
+    // Method to add a new recommendation
+    public void addRecommendation(Recommendation recommendation) {
+        recommendationRepository.save(recommendation);
+    }
+
+    // Method to get all recommendations
+    public List<Recommendation> getAllRecommendations() {
+        return new ArrayList<>(recommendationRepository.findAll());
+    }
+
+    // Method to get all recommendations of a given user
+    public List<Recommendation> getRecommendationsById(Integer userId) {
+        return recommendationRepository.findAll().stream().filter(x -> Objects.equals(x.getUserId(), userId)).collect(Collectors.toList());
     }
 }
